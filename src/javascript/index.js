d9c033c0e4696caa74ab3e533ecd7a6750fb5840
Pierrot <p.pinto2oliveira@gmail.com>
2020-10-13 23:18:50 +0200

    Spirales moving

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -18,8 +18,7 @@ let cH2 = (canvas.height / 2)
 
 let angle = 0
 let radius = 100
-
-let diff = 0
+let time = 0
 
 console.log(Math.cos(6), Math.sin(6))
 // à chaque image : 60fps
@@ -27,35 +26,31 @@ const update = () => {
     requestAnimationFrame(update)
 
     angle += .1
+    time += .1
 
     let mouseX = ((Mouse.cursor[0] + 1) / 2) * canvas.width
     let mouseY = ((Mouse.cursor[1] + 1) / 2) * canvas.height
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.save()
+
     ctx.translate(cW2, cH2)
-    ctx.beginPath()
-    for(let i = 0; i < 4; i++){
-        diff += .004
-        ctx.arc(Math.cos(angle) * radius + diff, Math.sin(angle) * radius + diff, 10, 0, Math.PI * 2)
-    }
-    ctx.fillStyle = 'white'
-    ctx.fill()
-    ctx.closePath()
-
-    ctx.beginPath()
-    ctx.fillStyle = '#ff0000'
-    ctx.arc(Math.cos(angle) * radius, 0, 5, 0, Math.PI * 2, true)
-    ctx.fill()
-    ctx.closePath()
-
-    ctx.beginPath()
-    ctx.fillStyle = '#00ff00'
-    ctx.arc(0, Math.sin(angle) * radius, 5, 0, Math.PI * 2, true)
-    ctx.fill()
-    ctx.closePath()
-
+    ctx.fillStyle = "#FFF"
+    for(let i = 0; i < 100; i++){
+        drawCircle(Math.PI * (i*0.1) + time, (i * 10 * Math.sin(time * .365)) + 20)    }
+    // drawCircle(0, 200, 'rgba(255, 0, 0, 1)')
+    // drawCircle(Math.PI / 2, 200, 'rgba(255, 0, 0, 1)')
+    // drawCircle(Math.PI, 200, 'rgba(255, 0, 0, 1)')
+    // drawCircle(- Math.PI / 2, 200, 'rgba(255, 0, 0, 1)')
+    // drawCircle(2.8, 500, 'rgba(255, 0, 0, 1)')
     ctx.restore()
-
 }
 requestAnimationFrame(update)
+
+function drawCircle(angle, radius, color) {
+    ctx.beginPath()
+    ctx.fillStyle = color
+    ctx.arc(Math.cos(angle + time) * radius, Math.sin(angle + time) * radius, 10, 0, Math.PI * 2)
+    ctx.fill()
+    ctx.closePath()
+}
