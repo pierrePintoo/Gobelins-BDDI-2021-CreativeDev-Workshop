c67940606d4418ac87549bf474e6fc4a8f93ffbe
pierrePintoo <p.pinto2oliveira@gmail.com>
2020-10-13 15:57:35 +0200

    Blob vite fait

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,6 +3,7 @@
 import Mouse from "./utils/mouse"
 import Easing from "./utils/easing"
 import Noise from "./utils/Noise"
+import Noise2D from "./utils/Noise2D"
 
 const canvas = document.querySelector('.main-canvas')
 const ctx = canvas.getContext("2d")
@@ -21,39 +22,59 @@ let maskLoaded = false
 
 let time = 0
 
-let noise = new Noise()
+let noise1D = new Noise()
+let simplex = new Noise2D()
 
 // à chaque image : 60fps
 const update = () => {
-    //requestAnimationFrame(update)
+    requestAnimationFrame(update)
 
     //time += .01
 
     //let mouseX = ((Mouse.cursor[0] + 1) / 2) * canvas.width
     //let mouseY = ((Mouse.cursor[1] + 1) / 2) * canvas.height
 
-    //ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     let size = 200
-
     let steps = 100
+    let stepsY = 1
+    let scaleY = .1
     let scale = 100
-    let frequency = .1
+    let frequency = .05
 
-    ctx.save()
+    time += .01
 
-    ctx.translate(0, cH2)
-    ctx.beginPath()
-    ctx.strokeStyle = '#FFF'
-    ctx.moveTo(0, noise.getVal(0) * scale)
-    console.log(noise.getVal(0))
-    for(let i = 0; i < steps; i++){
-        ctx.lineTo(i / steps * canvas.width, noise.getVal(i * frequency) * scale)
+    for (let y = 0; y < stepsY; y++) {
+        ctx.save()
+
+        ctx.translate(cW2, cH2)
+        ctx.beginPath()
+        ctx.strokeStyle = '#FFF'
+        /*ctx.moveTo(0, simplex.noise3D(0 + time, time, scaleY) * scale)
+        for(let i = 0; i < steps; i++){
+            ctx.lineTo(i / steps * canvas.width, simplex.noise3D(i * frequency + time * 2, time, y * scaleY) * scale)
+        }*/
+
+        let radius = 100
+        //ctx.moveTo(Math.cos(0) * radius, Math.sin(0) * radius)
+
+        for (let x = 0; x <= steps; x++){
+            let angle = (x / steps) * Math.PI * 2
+            radius = 200 + simplex.noise4D(Math.cos(angle), Math.sin(angle), y * scaleY, time) * scale + y * 100
+            ctx.lineTo(Math.cos((x / steps) * Math.PI * 2) * radius, Math.sin((x / steps) * Math.PI * 2) * radius)
+        }
+
+        radius = 200 + simplex.noise4D(Math.cos(0), Math.sin(0), y * scaleY, time) * scale + y * 100
+        ctx.lineTo(Math.cos(0) * radius, Math.sin(0) * radius)
+
+        ctx.stroke()
+        ctx.closePath()
+    
+        ctx.restore()
     }
-    ctx.stroke()
-    ctx.closePath()
 
-    ctx.restore()
+
 
 }
 requestAnimationFrame(update)
